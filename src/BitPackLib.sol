60c59328ce7b08109b66b0641cc93755c91a049e
kadenzipfel <kaden.zipfel@hotmail.com>
2022-07-23 16:13:17 -0700

    packBool

--- src/BitPackLib.sol
+++ src/BitPackLib.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-library BitPackLib {}
+error InvalidBitIndex(); 
+
+library BitPackLib {
+    function packBool(bytes32 word, bool value, uint256 bitIndex) internal pure returns (bytes32 newWord, uint256 freeBitIndex) {
+        if (bitIndex > 255) revert InvalidBitIndex();
+
+        assembly {
+            let shift := sub(255, bitIndex)
+            newWord := or(word, shl(shift, value))
+            freeBitIndex := add(bitIndex, 1)
+        }
+    }
+}
--- test/BitPackLib.t.sol
+++ test/BitPackLib.t.sol
@@ -2,11 +2,25 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/BitPackLib.sol";
+
+contract BitPackLibImpl {
+    function packBool(bytes32 word, bool value, uint256 bitIndex) public pure returns (bytes32 newWord, uint256 freeBitIndex) {
+        return BitPackLib.packBool(word, value, bitIndex);
+    }
+}
 
 contract BitPackLibTest is Test {
-    function setUp() public {}
+    BitPackLibImpl bpli;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        bpli = new BitPackLibImpl();
+    }
+
+    function testPackBool() public {
+        (bytes32 newWord, uint256 freeBitIndex) = bpli.packBool(bytes32(0), true, 0);
+
+        assertEq(newWord, 0x8000000000000000000000000000000000000000000000000000000000000000);
+        assertEq(freeBitIndex, 1);
     }
 }
