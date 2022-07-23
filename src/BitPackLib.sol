9b64fa2cf450c4a03d8faa998b0a35893e7ba834
kadenzipfel <kaden.zipfel@hotmail.com>
2022-07-23 16:52:44 -0700

    Remove unnecessary valid index check

--- src/BitPackLib.sol
+++ src/BitPackLib.sol
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-error InvalidBitIndex(); 
-
 library BitPackLib {
     // ============================================================
     //                        PACK METHODS
     // ============================================================
     
     function packBool(bytes32 word, bool value, uint256 bitIndex) internal pure returns (bytes32 newWord, uint256 freeBitIndex) {
-        if (bitIndex > 255) revert InvalidBitIndex();
-
         assembly {
             let shift := sub(255, bitIndex)
             newWord := or(word, shl(shift, value))
